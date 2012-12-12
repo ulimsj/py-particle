e922c2fa5724e22817d3d6b60b31191867119fcb
Leif Johnson <leif@leifjohnson.net>
2012-12-11 18:07:53 -0600

    Add README.md to package manifest ; update setup.py to reflect best packaging know-how to date.

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
+import os
 import setuptools
 
 setuptools.setup(
     name='lmj.particle',
-    version='0.1',
-    install_requires=['numpy'],
+    version='0.1.1',
     namespace_packages=['lmj'],
     packages=setuptools.find_packages(),
     author='Leif Johnson',
     author_email='leif@leifjohnson.net',
-    description='',
-    long_description=open('README.md').read(),
+    description='A library of generic particle filters',
+    long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')).read(),
     license='MIT',
+    url='http://github.com/lmjohns3/py-particle/',
     keywords=('particle '
               'filter '
               'probabilistic '
               'modeling '
               'machine '
               'learning'),
-    url='http://github.com/lmjohns3/py-particle/',
+    install_requires=['numpy'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
